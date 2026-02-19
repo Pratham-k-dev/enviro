cd9c68f49cc7f7ec3b4f92df55f10b7006b4700b
Pratham-k-dev <pratham.k3007@gmail.com>
2026-02-19 17:27:31 +0530

    callback

---
+++ app/auth/callback/page.js
@@ -0,0 +1,9 @@
+// app/auth/callback/page.js
+
+export default function AuthCallbackPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <p>Completing authentication... please wait.</p>
+    </div>
+  );
+}
